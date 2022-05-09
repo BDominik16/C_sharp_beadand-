6d4559a4bc227c1d981358df85cc3d2d7ca2b3fe
BDominik16 <78854621+BDominik16@users.noreply.github.com>
2022-05-09 19:10:52 +0200

    Add files via upload

---
+++ Bolgár_Dominik_AAH5X1_beadando/Bolgár_Dominik_AAH5X1_beadando/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Bolgár_Dominik_AAH5X1_beadando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SortedDictionary<string, string> felhasznalok = new SortedDictionary<string, string>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (i<10)
+                {
+                    felhasznalok.Add($"AHX{i}Z4", "Bolgár Dominik");
+                }
+                else
+                {
+                    felhasznalok.Add($"BHX{i}Z", "Bolgár Dominika");
+                }
+            }
+
+            sorbarendezett<string> sortedlista = new sorbarendezett<string>(felhasznalok);
+
+            foreach (var felhasznalo in sortedlista.Neptunfelhasznalok)
+            {
+                Console.WriteLine($"A felhasználók: {felhasznalo.Key}, {felhasznalo.Value}");
+
+            }
+
+            
+
+            if (sortedlista.Neptunfelhasznalok.ContainsKey("AHX5Z4"))
+            {
+                Console.WriteLine("Az AHX5Z4 neptunkód benne van a sorted dictionarybe.");
+            }
+            else
+            {
+                Console.WriteLine("Nincs benne az AHX5Z4 neptunkód a sorted dictionarybe.");
+            }
+
+            sortedlista.Neptunfelhasznalok.Remove("AHX5Z4");
+            Console.WriteLine("Az AH5XZ4 neptunkód ki lett törölve a sorted dictionaryból.");
+
+            sortedlista.Neptunfelhasznalok.Clear();
+            Console.WriteLine("Ki lett ürítve a dictionary!");
+       
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Bolgár_Dominik_AAH5X1_beadando/Bolgár_Dominik_AAH5X1_beadando/sorbarendezett.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bolgár_Dominik_AAH5X1_beadando
+{
+    class sorbarendezett<T> where T : System.IComparable<T> 
+    {
+        private SortedDictionary<string, string> neptunfelhasznalok;
+
+        public sorbarendezett(SortedDictionary<string, string> neptunfelhasznalok)
+        {
+            this.neptunfelhasznalok = neptunfelhasznalok;
+        }
+
+        public SortedDictionary<string, string> Neptunfelhasznalok { get => neptunfelhasznalok; set => neptunfelhasznalok = value; }
+        
+
+
+    }
+}
